c4215a841dd397df458658c3301e0766885c2cdf
Yash Rajak <140382057+yashrajak12@users.noreply.github.com>
2026-01-02 23:33:07 +0530

    Update config.py

--- hotel_management-3-render/app/config.py
+++ hotel_management-3-render/app/config.py
@@ -1,4 +1,10 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,           
+        "pool_recycle": 300,             
+    }
