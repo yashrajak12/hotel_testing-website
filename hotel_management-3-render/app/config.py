d52b698dcad7b1053ddf820498b6ca8b42c6ebcd
Yash Rajak <140382057+yashrajak12@users.noreply.github.com>
2026-01-03 11:20:00 +0530

    Update config.py

--- hotel_management-3-render/app/config.py
+++ hotel_management-3-render/app/config.py
@@ -1,22 +1,18 @@
-# config.py
 import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_pre_ping": True,     
-        "pool_recycle": 300,      
+        "pool_pre_ping": True,
+        "pool_recycle": 300,
         "pool_size": 5,
         "max_overflow": 10,
         "connect_args": {"sslmode": "require"}
     }
 
     SECRET_KEY = os.getenv('SECRET_KEY') or 'fallback-secret-key-for-development-only-do-not-use-in-production'
-
-
